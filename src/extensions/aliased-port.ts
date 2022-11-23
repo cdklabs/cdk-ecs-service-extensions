1e7634e1d9bdb2cd346ad3dc6241bf3bc89c681e
Austin Ely <austiely@amazon.com>
2022-11-23 18:06:34 +0000

    chore: add test coverage

--- src/extensions/aliased-port.ts
+++ src/extensions/aliased-port.ts
@@ -11,11 +11,6 @@ import { ContainerMutatingHook, ServiceBuild, ServiceExtension } from './extensi
  */
 export interface AliasedPortProps {
   /**
-  * The port on the container to expose to dns connections.
-  */
-  readonly containerPort?: number;
-
-  /**
    * The DNS alias to advertise for downstream clients.
    */
   readonly alias: string;
@@ -46,57 +41,55 @@ export interface AliasedPortProps {
    * @default no logging.
    */
   readonly logDriver?: ecs.LogDriver;
+
+  /**
+   * The new port mapping to create on the container. This is used when exposing multiple ports via Service Connect.
+   * This property will cause a new port mapping to be injected into the container and advertised via service connect.
+   */
+  readonly portMapping?: ecs.PortMapping;
 }
 
 export class AliasedPortExtension extends ServiceExtension {
-  protected containerPort: number;
   protected aliasDnsName: string;
-  protected aliasPort: number;
+  protected aliasPort?: number;
   protected discoveryName?: string;
   protected namespace?: cloudmap.INamespace;
   protected appProtocol?: ecs.AppProtocol;
   protected logDriver?: ecs.LogDriver;
 
-  protected parentServiceContainer: Container;
-  protected createNewPortMapping: boolean;
+  protected newPortMapping?: ecs.PortMapping;
 
   constructor(props: AliasedPortProps) {
     super('aliasedPort');
 
-    // Find the container extension to get its port.
-    const containerextension = this.parentService.serviceDescription.get('service-container') as Container;
-
-    if (!containerextension || !containerextension.container) {
-      throw new Error('Aliased port extension requires a container extension.');
-    }
-    this.parentServiceContainer = containerextension;
-
-    let trafficPort: number = this.parentServiceContainer.trafficPort;
-    this.containerPort = trafficPort;
-    this.createNewPortMapping = false;
-    if (props.containerPort && props.containerPort != trafficPort) {
-      this.containerPort = props.containerPort;
-      this.createNewPortMapping = true;
-    }
-
-
-    if (props.aliasTrafficPort) {
-      trafficPort = props.aliasTrafficPort;
-    };
-
+    this.aliasPort = props.aliasTrafficPort;
+    this.logDriver = props.logDriver;
     this.aliasDnsName = props.alias;
-    this.aliasPort = trafficPort;
+    this.aliasPort = props.aliasTrafficPort;
 
     this.discoveryName = props.discoveryName;
     this.appProtocol = props.protocol;
-    this.logDriver = props.logDriver;
+
+    this.newPortMapping = props.portMapping;
   }
 
+  private validatePortMappingForAliasedPortExtension(pm: ecs.PortMapping | undefined) {
+    if (!pm) {
+      return;
+    }
+    if (!pm.name) {
+      throw new Error('Port mapping must have a name.');
+    }
+    if (pm.protocol && pm.protocol !== ecs.Protocol.TCP) {
+      throw new Error('Port mapping must use TCP');
+    }
+  }
 
   public prehook(service: Service, scope: Construct) {
     this.parentService = service;
     this.scope = scope;
 
+    if 
     // If there isn't a default cloudmap namespace on the cluster, create a private HTTP namespace for SC.
     if (!this.parentService.cluster.defaultCloudMapNamespace) {
       this.parentService.environment.addDefaultCloudMapNamespace({
@@ -108,67 +101,70 @@ export class AliasedPortExtension extends ServiceExtension {
   }
 
   public addHooks(): void {
+    const containerextension = this.parentService.serviceDescription.get('service-container') as Container;
+    if (!containerextension) {
+      throw new Error('Aliased Port extension requires a Container Extension to already exist.');
+    }
+    this.validatePortMappingForAliasedPortExtension(this.newPortMapping);
 
-    // Container extension must have a container. The default protocol is TCP.
-    if (!this.parentServiceContainer.container) {
-      throw new Error('Container Extension must have a default container.');
+    if (this.newPortMapping && this.newPortMapping.name) {
+      containerextension.addContainerMutatingHook(new AliasedPortMutatingHook({
+        portMappingName: this.newPortMapping.name,
+        trafficPort: this.newPortMapping.containerPort,
+        protocol: this.newPortMapping.appProtocol,
+      }));
+    } else {
+      containerextension.addContainerMutatingHook(new AliasedPortMutatingHook({
+        portMappingName: this.aliasDnsName,
+        trafficPort: containerextension.trafficPort,
+        protocol: this.appProtocol,
+      }));
     }
-    var webPortMapping = this.parentServiceContainer.container.findPortMapping(this.parentServiceContainer.trafficPort, ecs.Protocol.TCP);
-    if (!webPortMapping) {
-      throw new Error(`Port ${this.parentServiceContainer.trafficPort} does not exist on the given container.`);
-    }
-    this.parentServiceContainer.addContainerMutatingHook(new AliasedPortMutatingHook({
-      portMappingName: this.aliasDnsName,
-      containerPort: this.containerPort,
-      protocol: this.appProtocol,
-      newPortMapping: this.createNewPortMapping,
-    }));
   }
 
   public modifyServiceProps(props: ServiceBuild): ServiceBuild {
-    if (props.serviceConnectConfiguration && props.serviceConnectConfiguration.namespace != this.namespace) {
+    if (props.serviceConnectConfiguration && props.serviceConnectConfiguration.namespace !== this.namespace) {
       throw new Error('Service connect cannot be enabled with two different namespaces.');
     }
-    const ns = this.namespace? this.namespace : this.parentService.cluster.defaultCloudMapNamespace;
-    if (!ns) {
-      throw new Error('Cloudmap namespace must be specified in either properties or environment.');
-    }
-    if (!this.parentServiceContainer.container) {
+
+    const containerextension = this.parentService.serviceDescription.get('service-container') as Container;
+
+    if (!containerextension.container) {
       throw new Error('Parent service must have a container to enable an Aliased Port Extension.');
     }
+    if (!containerextension.trafficPort && !this.aliasPort) {
+      throw new Error('Cannot infer port: container has no traffic port and aliasPort was not specified.');
+    }
+
+    const portMappingName = this.newPortMapping ? this.newPortMapping.name! : this.aliasDnsName;
+    const aliasPort = this.aliasPort ? this.aliasPort : this.newPortMapping ? this.newPortMapping.containerPort : containerextension.trafficPort;
+    // If there is already a service connect config, we need to modify the existing properties instead of creating new ones.
+    // Push a new service to the list of services.
+    let services: ecs.ServiceConnectService[] = [];
+    if (props.serviceConnectConfiguration) {
+      services = props.serviceConnectConfiguration.services ? props.serviceConnectConfiguration.services : [];
+    }
+    services.push({
+      port: portMappingName,
+      discoveryName: this.discoveryName,
+      aliases: [
+        {
+          port: aliasPort,
+          dnsName: this.aliasDnsName,
+        },
+      ],
+    });
     if (!props.serviceConnectConfiguration) {
       return {
         ...props,
 
         serviceConnectConfiguration: {
           enabled: true,
-          namespace: ns,
-          services: [
-            {
-              port: this.aliasDnsName,
-              discoveryName: this.discoveryName,
-              aliases: [
-                {
-                  dnsName: this.aliasDnsName,
-                  port: this.aliasPort,
-                },
-              ],
-            },
-          ],
+          namespace: this.namespace || this.parentService.cluster.defaultCloudMapNamespace,
+          services: services,
         },
       };
     }
-    let services: ecs.ServiceConnectService[] = props.serviceConnectConfiguration.services || [];
-    services.push({
-      port: this.aliasDnsName,
-      discoveryName: this.discoveryName,
-      aliases: [
-        {
-          port: this.aliasPort,
-          dnsName: this.aliasDnsName,
-        },
-      ],
-    });
     const scConfig = {
       ...props.serviceConnectConfiguration,
       services: services,
@@ -182,11 +178,20 @@ export class AliasedPortExtension extends ServiceExtension {
 }
 
 export interface AliasedPortMutatingHookProps {
-  readonly containerPort: number;
+  /**
+   * The name by which to refer to this port mapping.
+   */
   readonly portMappingName: string;
-  readonly trafficPort?: number;
+  /**
+   * The port on the container which receives traffic. This is the same as the `containerPort` property of port mapping.
+   */
+  readonly trafficPort: number;
+  /**
+   * The protocol which this port mapping expects to receive.
+   *
+   * @default: none
+   */
   readonly protocol?: ecs.AppProtocol;
-  readonly newPortMapping?: boolean;
 }
 
 /**
@@ -197,21 +202,16 @@ export class AliasedPortMutatingHook extends ContainerMutatingHook {
   private portMappingName: string;
   private portMappingProtocol?: ecs.AppProtocol;
   private trafficPort: number;
-  private createNewPortMapping: boolean;
 
   constructor(props: AliasedPortMutatingHookProps) {
     super();
     this.portMappingName = props.portMappingName;
-    this.trafficPort = props.containerPort;
-    if (props.trafficPort) {
-      this.trafficPort = props.trafficPort;
-    }
+    this.trafficPort = props.trafficPort;
     this.portMappingProtocol = props.protocol;
-    this.createNewPortMapping = props.newPortMapping ? props.newPortMapping : false;
   }
 
   public mutateContainerDefinition(props: ecs.ContainerDefinitionOptions): ecs.ContainerDefinitionOptions {
-    return this.createNewPortMapping ? {
+    return {
       ...props,
 
       portMappings: [
@@ -221,6 +221,6 @@ export class AliasedPortMutatingHook extends ContainerMutatingHook {
           appProtocol: this.portMappingProtocol,
         },
       ],
-    } as ecs.ContainerDefinitionOptions : props;
+    } as ecs.ContainerDefinitionOptions;
   }
 }
--- test/aliased-port.test.ts
+++ test/aliased-port.test.ts
@@ -1,8 +1,6 @@
 import { Stack } from 'aws-cdk-lib';
-import { Match, Template } from 'aws-cdk-lib/assertions';
-import * as appmesh from 'aws-cdk-lib/aws-appmesh';
+import { Template } from 'aws-cdk-lib/assertions';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
-import { Alias } from 'aws-cdk-lib/aws-kms';
 import { AliasedPortExtension, Container, Environment, ServiceDescription, Service } from '../lib';
 
 describe('aliased port', () => {
@@ -13,6 +11,34 @@ describe('aliased port', () => {
     stack = new Stack();
     environment = new Environment(stack, 'production');
     serviceDescription = new ServiceDescription();
+  });
+
+  test('aliased port extension throws error when no container extension exists', () => {
+    expect(() => {
+      serviceDescription.add(new AliasedPortExtension({
+        alias: 'name',
+      }));
+    }).toThrow('Aliased Port extension requires a Container Extension to already exist.');
+  });
+
+  test('service connect cannot be enabled with two different namespaces.', () => {
+    serviceDescription.add(new Container({
+      cpu: 256,
+      memoryMiB: 512,
+      trafficPort: 80,
+      image: ecs.ContainerImage.fromRegistry('nathanpeck/name'),
+    }));
+    serviceDescription.add(new AliasedPortExtension({
+      alias: 'direct', 
+    });
+    serviceDescription.add(new AliasedPortExtension({
+      alias: 'indirect',
+      portMapping: {
+        containerPort: 8080,
+        name: 'indirect', 
+        appProtocol: ecs.AppProtocol.GRPC,
+      },
+    }));
   });
 
   test('when adding an aliased port with minimal config', () => {
@@ -33,6 +59,22 @@ describe('aliased port', () => {
     });
 
     // THEN
-    Template.fromStack(stack).hasResourceProperties('AWS::ECS::Service');
+    Template.fromStack(stack).hasResourceProperties('AWS::ECS::Service', {
+      ServiceConnectConfiguration: {
+        Enabled: true,
+        Namespace: 'production',
+        Services: [
+          {
+            PortName: 'name',
+            ClientAliases: [
+              {
+                Port: 80,
+                DnsName: 'name',
+              },
+            ],
+          },
+        ],
+      },
+    });
   });
 });
